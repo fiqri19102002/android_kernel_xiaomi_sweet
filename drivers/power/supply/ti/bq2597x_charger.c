38b9fb31a63d14bf4d13800ac471ccbfe9f3ebf8
Adam W. Willis <return.of.octobot@gmail.com>
2020-12-20 15:24:47 -0500

    drivers: power: bq2597x_charger: Comment out unused functions
    This silences the following compiler warnings:
    
    ../drivers/oneplus/power/supply/wlchg/bq2597x_charger.c:412:12: warning: unused function 'bq2597x_set_wdt' [-Wunused-function]
    static int bq2597x_set_wdt(struct bq2597x *bq, int ms)
               ^
    ../drivers/oneplus/power/supply/wlchg/bq2597x_charger.c:1037:12: warning: unused function 'bq2597x_clear_alarm_int_mask' [-Wunused-function]
    static int bq2597x_clear_alarm_int_mask(struct bq2597x *bq, u8 mask)
               ^
    
    ../drivers/oneplus/power/supply/wlchg/bq2597x_charger.c:1071:12: warning: unused function 'bq2597x_clear_fault_int_mask' [-Wunused-function]
    static int bq2597x_clear_fault_int_mask(struct bq2597x *bq, u8 mask)
               ^
    
    Signed-off-by: Adam W. Willis <return.of.octobot@gmail.com>
    Signed-off-by: Subhajeet Muhuri <subhajeet.muhuri@aosip.dev>
    Signed-off-by: Anush02198 <Anush.4376@gmail.com>
    Signed-off-by: Fiqri Ardyansyah <fiqri15072019@gmail.com>

--- drivers/power/supply/ti/bq2597x_charger.c
+++ drivers/power/supply/ti/bq2597x_charger.c
@@ -524,6 +524,7 @@ static int bq2597x_enable_wdt(struct bq2597x *bq, bool enable)
 }
 EXPORT_SYMBOL_GPL(bq2597x_enable_wdt);
 
+#if 0
 static int bq2597x_set_wdt(struct bq2597x *bq, int ms)
 {
 	int ret;
@@ -547,6 +548,7 @@ static int bq2597x_set_wdt(struct bq2597x *bq, int ms)
 	return ret;
 }
 EXPORT_SYMBOL_GPL(bq2597x_set_wdt);
+#endif
 
 static int bq2597x_enable_batovp(struct bq2597x *bq, bool enable)
 {
@@ -1181,6 +1183,7 @@ static int bq2597x_set_alarm_int_mask(struct bq2597x *bq, u8 mask)
 }
 EXPORT_SYMBOL_GPL(bq2597x_set_alarm_int_mask);
 
+#if 0
 static int bq2597x_clear_alarm_int_mask(struct bq2597x *bq, u8 mask)
 {
 	int ret;
@@ -1197,6 +1200,7 @@ static int bq2597x_clear_alarm_int_mask(struct bq2597x *bq, u8 mask)
 	return ret;
 }
 EXPORT_SYMBOL_GPL(bq2597x_clear_alarm_int_mask);
+#endif
 
 static int bq2597x_set_fault_int_mask(struct bq2597x *bq, u8 mask)
 {
@@ -1215,6 +1219,7 @@ static int bq2597x_set_fault_int_mask(struct bq2597x *bq, u8 mask)
 }
 EXPORT_SYMBOL_GPL(bq2597x_set_fault_int_mask);
 
+#if 0
 static int bq2597x_clear_fault_int_mask(struct bq2597x *bq, u8 mask)
 {
 	int ret;
@@ -1231,6 +1236,7 @@ static int bq2597x_clear_fault_int_mask(struct bq2597x *bq, u8 mask)
 	return ret;
 }
 EXPORT_SYMBOL_GPL(bq2597x_clear_fault_int_mask);
+#endif
 
 static int sc8551_set_bypass_mode_en(struct bq2597x *bq, int enable)
 {
