7b3601507bf7837757279782f9a47342e0d29371
Dakkshesh <dakkshesh5@gmail.com>
2021-09-30 19:40:17 +0800

    drivers: devfreq_boost: Skip boosting if Profile is set to Battery Mode
    Signed-off-by: Dakkshesh <dakkshesh5@gmail.com>
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>
    [fiqri19102002: Adapt to new implementation and
    		fix build when CONFIG_KPROFILES is disabled]
    Signed-off-by: Fiqri Ardyansyah <fiqri15072019@gmail.com>

--- drivers/devfreq/devfreq_boost.c
+++ drivers/devfreq/devfreq_boost.c
@@ -34,6 +34,10 @@ struct df_boost_drv {
 	unsigned long last_input_jiffies;
 };
 
+#ifdef CONFIG_KPROFILES
+extern int kp_active_mode(void);
+#endif
+
 static void devfreq_input_unboost(struct work_struct *work);
 static void devfreq_max_unboost(struct work_struct *work);
 
@@ -59,6 +63,11 @@ static void __devfreq_boost_kick(struct boost_dev *b)
 	if (!READ_ONCE(b->df) || test_bit(SCREEN_OFF, &b->state))
 		return;
 
+#ifdef CONFIG_KPROFILES
+	if (kp_active_mode() == 1)
+		return;
+#endif
+
 	set_bit(INPUT_BOOST, &b->state);
 	if (!mod_delayed_work(system_unbound_wq, &b->input_unboost,
 		msecs_to_jiffies(CONFIG_DEVFREQ_INPUT_BOOST_DURATION_MS))) {
@@ -82,6 +91,11 @@ static void __devfreq_boost_kick_max(struct boost_dev *b,
 
 	if (!READ_ONCE(b->df) || test_bit(SCREEN_OFF, &b->state))
 		return;
+
+#ifdef CONFIG_KPROFILES
+	if (kp_active_mode() == 1)
+		return;
+#endif
 
 	boost_jiffies = msecs_to_jiffies(duration_ms);
 	do {
