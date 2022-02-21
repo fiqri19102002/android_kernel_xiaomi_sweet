ad718e6ec6d6c6cd461e89043eb09b3f48e55a25
Nauval Rizky <enuma.alrizky@gmail.com>
2022-02-22 00:52:33 +0700

    power: smb5: Remove xiaomi disable_thermal sysfs
    I can't find anywhere even on stock ROM who is actually using this node,
    so better just nuke it to prevent someone from misusing this node.
    
    Signed-off-by: Nauval Rizky <enuma.alrizky@gmail.com>
    Signed-off-by: Fiqri Ardyansyah <fiqri0927936@gmail.com>

--- drivers/power/supply/qcom/qpnp-smb5.c
+++ drivers/power/supply/qcom/qpnp-smb5.c
@@ -260,11 +260,6 @@ module_param_named(
 );
 
 #ifdef CONFIG_MACH_XIAOMI_SWEET
-static bool disable_thermal = false;
-module_param_named(
-	disable_thermal, disable_thermal, bool, 0600
-);
-
 static int __mtbf_icl = 0;
 module_param_named(
 	mtbf_icl, __mtbf_icl, int, 0600
@@ -2454,10 +2449,6 @@ static int smb5_batt_set_prop(struct power_supply *psy,
 		rc = smblib_set_prop_input_suspend(chg, val);
 		break;
 	case POWER_SUPPLY_PROP_CHARGE_CONTROL_LIMIT:
-#ifdef CONFIG_MACH_XIAOMI_SWEET
-		if (disable_thermal)
-			break;
-#endif
 		rc = smblib_set_prop_system_temp_level(chg, val);
 		break;
 	case POWER_SUPPLY_PROP_CAPACITY:
