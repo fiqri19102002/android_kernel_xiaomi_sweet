f164349a3c8741d50a70304a37812ce97d0f90e7
Fiqri Ardyansyah <fiqri0927936@gmail.com>
2023-01-02 15:54:19 +0700

    build: Switch to CROSS_COMPILE_COMPAT
    Signed-off-by: Fiqri Ardyansyah <fiqri0927936@gmail.com>

--- build.sh
+++ build.sh
@@ -171,7 +171,7 @@ compile() {
 	if [ $COMPILER == "clang" ]; then
 		make -j"$PROCS" O=out \
 				CROSS_COMPILE=aarch64-linux-gnu- \
-				CROSS_COMPILE_ARM32=arm-linux-gnueabi- \
+				CROSS_COMPILE_COMPAT=arm-linux-gnueabi- \
 				CC=clang \
 				AR=llvm-ar \
 				NM=llvm-nm \
@@ -179,7 +179,7 @@ compile() {
 				OBJDUMP=llvm-objdump \
 				STRIP=llvm-strip
 	elif [ $COMPILER == "gcc" ]; then
-		export CROSS_COMPILE_ARM32=$GCC32_DIR/bin/arm-none-eabi-
+		export CROSS_COMPILE_COMPAT=$GCC32_DIR/bin/arm-none-eabi-
 		make -j"$PROCS" O=out CROSS_COMPILE=aarch64-none-elf-
 	fi
 	BUILD_END=$(date +"%s")
