f397f784883e2853182593a36f5d7d3ed61e2b6d
Fiqri Ardyansyah <fiqri0927936@gmail.com>
2023-05-16 11:03:39 +0700

    drivers/usb: Import Xiaomi changes
    * Based on sweet-r-oss
    
    Signed-off-by: Fiqri Ardyansyah <fiqri0927936@gmail.com>

--- drivers/usb/core/hub.c
+++ drivers/usb/core/hub.c
@@ -146,6 +146,9 @@ struct usb_hub *usb_hub_to_struct_hub(struct usb_device *hdev)
 int usb_device_supports_lpm(struct usb_device *udev)
 {
 	/* Some devices have trouble with LPM */
+#ifdef CONFIG_MACH_XIAOMI_SWEET
+	return 0;
+#endif
 	if (udev->quirks & USB_QUIRK_NO_LPM)
 		return 0;
 
@@ -4421,6 +4424,7 @@ static int hub_set_address(struct usb_device *udev, int devnum)
 	return retval;
 }
 
+#ifndef CONFIG_MACH_XIAOMI_SWEET
 /*
  * There are reports of USB 3.0 devices that say they support USB 2.0 Link PM
  * when they're plugged into a USB 2.0 port, but they don't work when LPM is
@@ -4447,6 +4451,7 @@ static void hub_set_initial_usb2_lpm_policy(struct usb_device *udev)
 		usb_enable_usb2_hardware_lpm(udev);
 	}
 }
+#endif
 
 static int hub_enable_device(struct usb_device *udev)
 {
@@ -4797,7 +4802,9 @@ hub_port_init(struct usb_hub *hub, struct usb_device *udev, int port1,
 	/* notify HCD that we have a device connected and addressed */
 	if (hcd->driver->update_device)
 		hcd->driver->update_device(hcd, udev);
+#ifndef CONFIG_MACH_XIAOMI_SWEET
 	hub_set_initial_usb2_lpm_policy(udev);
+#endif
 fail:
 	if (retval) {
 		hub_port_disable(hub, port1, 0);
