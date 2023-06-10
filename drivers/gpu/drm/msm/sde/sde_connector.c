76032f2598b55e828d1f34522d76986c8cecbfe0
Fiqri Ardyansyah <fiqri0927936@gmail.com>
2023-06-10 16:29:24 +0700

    drivers: drm: Restore some changes
    Signed-off-by: Fiqri Ardyansyah <fiqri0927936@gmail.com>

--- drivers/gpu/drm/msm/sde/sde_connector.c
+++ drivers/gpu/drm/msm/sde/sde_connector.c
@@ -96,6 +96,11 @@ static int sde_backlight_device_update_status(struct backlight_device *bd)
 	if (!bl_lvl && brightness)
 		bl_lvl = 1;
 
+#ifdef CONFIG_MACH_XIAOMI_SWEET
+	if (bl_lvl && bl_lvl < display->panel->bl_config.bl_min_level)
+		bl_lvl = display->panel->bl_config.bl_min_level;
+#endif
+
 	if (!c_conn->allow_bl_update) {
 		c_conn->unset_bl_level = bl_lvl;
 		return 0;
